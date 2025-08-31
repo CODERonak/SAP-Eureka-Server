9c5d657812c338f677ecbe0fbe06573fed2d4829
Firebase Studio <noreply@firebase.studio>
2025-08-31 09:49:45 +0000

    Initialized workspace with Firebase Studio

---
+++ src/main/java/com/microservice/SAPEurekaServer/SapEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.SAPEurekaServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SapEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SapEurekaServerApplication.class, args);
+	}
+
+}
