fb8285218cdffdfdb982ee70e6c2fb009557ecf0
r9988.2588 <r9988.2588@gmail.com>
2025-09-02 04:40:37 +0000

    first commit

--- src/main/java/com/microservice/SAPEurekaServer/SapEurekaServerApplication.java
+++ src/main/java/com/microservice/SAPEurekaServer/SapEurekaServerApplication.java
@@ -2,10 +2,11 @@ package com.microservice.SAPEurekaServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class SapEurekaServerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SapEurekaServerApplication.class, args);
 	}
